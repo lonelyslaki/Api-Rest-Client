e0451c8ddf11ca9b9ff4f1f3dcf14e6f96e647d0
Fernando Valdivia <73622787+lonelyslaki@users.noreply.github.com>
2023-12-07 01:59:34 -0300

    Add files via upload

---
+++ Migrations/20231207015243_AddEmailToClient.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApiProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailToClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Clients");
+        }
+    }
+}
---
+++ Migrations/20231207015558_AddCorreoToClient.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApiProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCorreoToClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Clients",
+                newName: "Numero");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Clients",
+                newName: "Nombre");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Correo",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Edad",
+                table: "Clients",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Correo",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Edad",
+                table: "Clients");
+
+            migrationBuilder.RenameColumn(
+                name: "Numero",
+                table: "Clients",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Nombre",
+                table: "Clients",
+                newName: "Email");
+        }
+    }
+}
